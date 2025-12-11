89fda1bceee25964d639b983602e4c15ba9ed449
Saggy <saggy.ohad@gmail.com>
2025-12-11 17:51:14 +0200

    done part A

---
+++ DebugWorkshop/MyPointExactly/main.cpp
@@ -0,0 +1,15 @@
+#include "Point.h"
+#include <iostream>
+
+
+int main()
+{
+	Point p1(1, 2);
+	Point p2;
+	p2 = p1;
+
+	std::cout << "p1=" << p1 << std::endl; // i added "<< p1" to fix the problem
+	std::cout << "p2=" << p2 << std::endl; // i added "<< p2" to fix the problem
+
+	return 0;
+}
