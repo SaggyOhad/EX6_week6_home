f6415ad62e25eb815302f7c54d6817a25d55f18e
Saggy <saggy.ohad@gmail.com>
2025-12-11 18:46:10 +0200

    done part C

--- DebugWorkshop/MyPointExactly/Point.cpp
+++ DebugWorkshop/MyPointExactly/Point.cpp
@@ -16,18 +16,22 @@ Point::Point(int x, int y)
 
 Point::Point(const Point& other)
 {
-	delete _coord;
-	_coord = other._coord;
+	/*
+	* The solution to double voting
+	*/
+	_coord = new int[2];
+	_coord[0] = other._coord[0];
+	_coord[1] = other._coord[1];
 }
 
 Point::~Point()
 {
-	delete _coord;
+	delete[] _coord; // add []
 }
 
 Point& Point::operator=(const Point& other)
 {
-	delete _coord;
+	delete[] _coord; // add []
 	_coord = new int[2];
 	memcpy(_coord, other._coord, 8); // switch 2 to 8 bc it's 8 byets and not 2
 	return *this;
--- DebugWorkshop/MyPointExactly/main.cpp
+++ DebugWorkshop/MyPointExactly/main.cpp
@@ -5,8 +5,13 @@
 int main()
 {
 	Point p1(1, 2);
+	// for part c changes
+	/*
 	Point p2;
 	p2 = p1;
+	*/
+
+	Point p2 = p1;
 
 	std::cout << "p1=" << p1 << std::endl; // i added "<< p1" to fix the problem
 	std::cout << "p2=" << p2 << std::endl; // i added "<< p2" to fix the problem
